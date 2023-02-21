4f8304680bb403193d3660428190cc47bf4c6561
Madmadi <madmadi@Mohammads-MacBook-Pro.local>
2023-02-21 21:50:49 +0330

    add help menu

--- ContatsList/index.js
+++ ContatsList/index.js
@@ -7,22 +7,46 @@ const contactsList = [];
 
 console.log('--- ContactsList ---');
 
-const firstName = await rl.question('First Name: ');
-const lastName = await rl.question('Last Name: ');
+async function addNewContact() {
+    const firstName = await rl.question('First Name: ');
+    const lastName = await rl.question('Last Name: ');
+    
+    const newContact = {
+        id: contactsList.length,
+        firstName,
+        lastName,
+    };
+    
+    contactsList.push(newContact);
+}
 
-const newContact = {
-    id: contactsList.length,
-    firstName,
-    lastName,
-};
+function showContactsList() {
+    const formattedContactsList = contactsList
+        .map(({ id, firstName, lastName }) => `#${id} ${firstName} ${lastName}`)
+        .join('\n');
+    
+    console.log('Contacts List:');
+    console.log(formattedContactsList);
+}
 
-contactsList.push(newContact);
+function quit() {
+    rl.close();
+}
 
-const formattedContactsList = contactsList
-    .map(({ id, firstName, lastName }) => `#${id} ${firstName} ${lastName}`)
-    .join('\n');
+async function help() {
+    console.log('n: Add new contact\nl: show contacts list\nq: quit');
+    const action = await rl.question('Enter your input: ');
 
-console.log('Contacts List:');
-console.log(formattedContactsList);
+    if (action === 'n') {
+        await addNewContact();
+    } else if (action === 'l') {
+        showContactsList();
+    } else {
+        quit();
+        return;
+    }
 
-rl.close();
+    help();
+}
+
+help();
