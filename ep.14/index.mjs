f9fc655eb5582e01cee8f8f3d84626c54fa434ff
Madmadi <madmadi@Mohammads-MacBook-Pro.local>
2023-03-17 17:30:24 +0330

    get name query param using HTTP module

---
+++ ep.14/index.mjs
@@ -0,0 +1,18 @@
+import http from 'http';
+import url from 'url';
+
+const server = http.createServer((req, res) => {
+    const urlData = url.parse(req.url, true);
+    const { name } = urlData.query;
+
+    const message = name
+        ? `Hello ${name}!`
+        : 'Hello from NodeJS HTTP server!';
+
+    res.write(message);
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('HTTP server is listening on the port 3000');
+});
