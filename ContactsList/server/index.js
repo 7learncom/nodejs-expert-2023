63335b3a87b64bafabcf7047f2ec024c2c664d58
Madmadi <madmadi@Mohammads-MacBook-Pro.local>
2023-04-23 21:46:03 +0330

    refactor server.js to move routes to another file

---
+++ ContactsList/server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes.js';
+
+const app = express();
+
+function loggerMiddleware(req, res, next) {
+    console.log('Request:', req.method, req.url);
+    next();
+}
+
+app.disable('etag');
+app.use(loggerMiddleware);
+app.use('/contacts', routes);
+app.listen(3000, () => {
+    console.log('express server is listening on the port 3000');
+});
