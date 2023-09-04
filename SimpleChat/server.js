222252917692c3a2d931088b2361871424937c6c
Madmadi <madmadi@Mohammads-MacBook-Pro.local>
2023-09-04 19:28:56 +0330

    create SimpleChat project

---
+++ SimpleChat/server.js
@@ -0,0 +1,12 @@
+import { Server } from 'socket.io';
+import { EVENT_INFO } from './constants.js';
+
+const io = new Server(3000);
+
+console.log('Server is started...');
+
+io.on('connection', (socket) => {
+    console.log('A client has been connected');
+
+    socket.emit(EVENT_INFO, 'Welcome to SimpleChat');
+});
